17e68e3ff05ffb39fd5216b847887e1bf12238f2
Steven Hatch <90797071+stevegreghatch@users.noreply.github.com>
2021-09-28 14:17:34 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@ int main() {
 	"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
 	"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
 	"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-	"A5,Steven,Hatch,shatc55@wgu.edu,28,21,10,11,SOFTWARE"
+	"A5,Steven,Hatch,--,28,21,10,11,SOFTWARE"
 	};
 
 	//print out to the screen, via your application, the course title, the programming language used, your WGU student ID, and your name.
 	cout << "Course Title: C867 Scripting and Programming Applications" << endl;
 	cout << "Programming Language Used: C++" << endl;
-	cout << "WGU Student ID: 001396379" << endl;
+	cout << "WGU Student ID: --" << endl;
 	cout << "Student Name: Steven Hatch" << endl;
 	cout << endl;
 
